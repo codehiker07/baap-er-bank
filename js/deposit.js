c4aa67df74527e9cd935b0d07513bde315f9e9f8
codehiker07 <codehiker07@gmail.com>
2023-08-15 15:04:03 +0600

    Almost Done all Function

--- js/deposit.js
+++ js/deposit.js
@@ -1,35 +1,64 @@
-
-
-/* const initialWithdraw = document.getElementById('initial-withdraw');
-const withdraw = initialWithdraw.innerText;
-
-const initialBalance = document.getElementById('initial-balance');
-const balance = initialBalance.innerText;
-
-// console.log(deposit, withdraw, balance); */
-
+//Deposit Side
 document.getElementById('btn-deposit').addEventListener('click', function () {
-    // console.log('clicked');
-    const deposit = document.getElementById('deposit-id');
-    const depositAmount = deposit.value;
+    // Please Deposit Section
+    const pleaseDeposit = document.getElementById('deposit-id');
+    const depositAmount = pleaseDeposit.value;
+    const parseDepositAmount = parseFloat(depositAmount);
+    // console.log(typeof parseDepositAmount);
 
+    //Total Deposit Section (Previous)
     const initialDeposit = document.getElementById('initial-deposit');
-    const depositTotal = initialDeposit.innerText;
-    initialDeposit.innerText = depositAmount;
-
-
+    const initialDepositTotal = initialDeposit.innerText;
+    const parsePreviousDepositTotal = parseFloat(initialDepositTotal);
+    //Total Deposit Section (Current)
+    const currentDepositTotal = parsePreviousDepositTotal + parseDepositAmount;
+    initialDeposit.innerText = currentDepositTotal;
 })
 
-
-
-
-
-
-
-/* // Withdraw section
+//Withdraw Side
 document.getElementById('btn-withdraw').addEventListener('click', function () {
-    // console.log('clicked');
-    const withdraw = document.getElementById('withdraw-id');
-    const withdrawAmount = withdraw.value;
-    // console.log(withdrawAmount);
-}) */
+    // Please Withdraw Section
+    const pleaseWithdraw = document.getElementById('withdraw-input');
+    const withdrawAmount = pleaseWithdraw.value;
+    const parseWithdrawAmount = parseFloat(withdrawAmount);
+    // console.log(typeof parseWithdrawAmount);
+
+    //Total Withdraw Section (Previous)
+    const initialWithdraw = document.getElementById('initial-withdraw');
+    const initialWithdrawTotal = initialWithdraw.innerText;
+    const parsePreviousWithdrawTotal = parseFloat(initialWithdrawTotal);
+    //Total Withdraw Section (Current)
+    const currentWithdrawTotal = parsePreviousWithdrawTotal - parseWithdrawAmount;
+    initialWithdraw.innerText = currentWithdrawTotal;
+})
+
+//Total Balance Section After Withdraw
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+    // Please Withdraw Section
+    const pleaseWithdraw = document.getElementById('withdraw-input');
+    const withdrawAmount = pleaseWithdraw.value;
+    const parseWithdrawAmount = parseFloat(withdrawAmount);
+    //Current Balance
+    const initialBalance = document.getElementById('initial-balance');
+    const initialBalanceAmount = initialBalance.innerText;
+    const parseInitialBalanceAmount = parseFloat(initialBalanceAmount);
+    //Balance Calculation
+    const balanceAfterWithdraw = parseInitialBalanceAmount - parseWithdrawAmount;
+    initialBalance.innerText = balanceAfterWithdraw;
+})
+
+//Total Balance Section After Deposit
+document.getElementById('btn-deposit').addEventListener('click', function () {
+    // Please Deposit Section
+    const pleaseDeposit = document.getElementById('deposit-id');
+    const depositAmount = pleaseDeposit.value;
+    const parseDepositAmount = parseFloat(depositAmount);
+    //Current Balance
+    const initialBalance = document.getElementById('initial-balance');
+    const initialBalanceAmount = initialBalance.innerText;
+    const parseInitialBalanceAmount = parseFloat(initialBalanceAmount);
+    //Balance Calculation
+    const balanceAfterDeposit = parseInitialBalanceAmount + parseDepositAmount;
+    initialBalance.innerText = balanceAfterDeposit;
+})
+
